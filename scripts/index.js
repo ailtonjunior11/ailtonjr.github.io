195f4b363777e6052df2bc301abce04a0eda90f0
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2024-08-15 08:44:42 -0300

    Atualizações

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,39 @@ window.addEventListener("scroll", function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add("active");
   return navbar.classList.remove("active");
 });
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".header__left", { distance: "50px", duration: 2000 });
+
+sr.reveal(".header__image", {
+  duration: 2000,
+  distance: "100px",
+  origin: "top",
+});
+
+sr.reveal(".about__left", {
+  distance: "100px",
+  origin: "left",
+  duration: 1800,
+});
+
+sr.reveal(".about__right", {
+  distance: "100px",
+  origin: "right",
+  duration: 2000,
+});
+
+sr.reveal(".li-1", { distance: "300px", origin: "left", duration: 1500 });
+
+sr.reveal(".li-2", { distance: "300px", origin: "right", duration: 1700 });
+
+sr.reveal(".li-3", { distance: "300px", origin: "left", duration: 1900 });
+
+sr.reveal(".li-4", { distance: "300px", origin: "right", duration: 2100 });
+
+sr.reveal(".skills__item", {
+  distance: "100px",
+  origin: "bottom",
+  duration: 3000,
+});
